619f1a8702addbfb567509e6da32cc123ed7ffa7
Daniele Monteleone <daniele@monteleone.ml>
2017-10-06 19:22:48 +0200

    test: remove tests from example

--- src/PackageDownloader.ts
+++
@@ -1,9 +0,0 @@
-import { Logger } from "./logger"
-import { PackageManager } from "./package"
-import { Platform } from "./platform";
-
-export async function installPackages(packageJSON: any, platform: Platform, logger: Logger) {
-  const packageManager = new PackageManager(packageJSON, platform, logger)
-  await packageManager.downloadPackages()
-  console.log("downloaded")
-}
